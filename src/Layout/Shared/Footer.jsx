cf32e3dc7fcbbe4f71b04a1ff82b2c65e6c659e6
shrabon <getshrabon@gmail.com>
2024-10-20 10:09:15 +0600

    Updated

--- src/Layout/Shared/Footer.jsx
+++ src/Layout/Shared/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1 className='text-3xl'>Footer</h1>
-        </div>
+       <footer>
+        
+       </footer>
     );
 };
 
