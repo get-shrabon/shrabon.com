a42cab8ebf8382823cd6bd67b2cc131525cf4750
shrabon <getshrabon@gmail.com>
2024-10-26 11:22:23 +0600

    Updated

--- src/Layout/Pages/HomePage/Banner.jsx
+++ src/Layout/Pages/HomePage/Banner.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import bannerImg from '../../../assets/Images/banner_img.png';
 
 const Banner = () => {
     return (
-        <div className='text-5xl min-h-[100vh] flex items-center'>
-            Banner
+        <div className=' min-h-[100vh] flex items-center home-banner bg-cover bg-center bg-no-repeat overflow-hidden'>
+            <div className="container mx-auto">
+                <div className='grid grid-cols-2'>
+                    <div className='w-[70%] banner-img relative'>
+                        <img className='w-full' src={bannerImg} alt="Banner" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
